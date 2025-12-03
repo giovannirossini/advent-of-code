cd7fcf94032c0dc319bfb03733de0e06b10c7a1f
Giovanni Rossini <giovannijrrossini@gmail.com>
2025-12-03 00:07:53 -0300

    day two: part two

--- day2.sh
+++ day2.sh
@@ -2,21 +2,46 @@
 
 input=($(tr ',' '\n' < $1))
 
-sum_invalids=0
+part_one=0
+part_two=0
+count=0
 
 for id in ${input[@]}; do
-    first_id="${id%-*}"
-    last_id="${id#*-}"
+    first="${id%-*}"
+    last="${id#*-}"
 
-    for ((i=first_id; i<=last_id; i++)) do
-        len=${#i}
+    echo "$((++count))/${#input[@]}"
+
+    for ((num=first; num<=last; num++)) do
+        len=${#num}
+        mid=$((len / 2))
+        
         if ((len % 2 == 0)); then
             half=$((len/2))
-            if [[ ${i:0:half} == ${i:half:len} ]]; then
-                ((sum_invalids += i))
+            if [[ ${num:0:half} == ${num:half:len} ]]; then
+                ((part_one += num))
             fi
         fi
+
+        for ((i=1; i<=mid; i++)); do
+            subsrt=${num:0:i}
+            
+            if ((len % i == 0)); then
+                x=$((len / i))
+
+                newstr=''
+                for ((j=0; j<x; j++)); do
+                    newstr+=$subsrt
+                done
+
+                if ((newstr == num)); then
+                    ((part_two += num ))
+                    continue 2
+                fi
+            fi
+        done
     done
 done
 
-echo "Total invalids are: $sum_invalids."
+echo "The sum of invalids for part one is: $part_one."
+echo "The sum of invalids for part two is: $part_two."
