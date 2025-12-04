dc577c46a06f9632b7d54afdb8c16899a0e91b18
Giovanni Rossini <giovannijrrossini@gmail.com>
2025-12-04 18:52:22 -0300

    day4: part two

--- day4.sh
+++ day4.sh
@@ -3,6 +3,7 @@
 grid=()
 accessible=0
 directions=("-1 -1" "-1 0" "-1 1" "0 -1" "0 1" "1 -1" "1 0" "1 1")
+reset="true"
 
 while read line; do
     grid+=("$line")
@@ -11,31 +12,41 @@ done < "$1"
 rows=${#grid[@]}
 cols=${#grid[0]}
 
-for ((i=0; i<rows; i++)); do
-    for ((j=0; j<cols; j++)); do
-        char=${grid[$i]:$j:1}
-        
-        count=0
-        if [[ "$char" == "@" ]]; then
-            for direction in "${directions[@]}"; do
-                read dir_row dir_col <<< "$direction"
-                next_row=$((i + dir_row))
-                next_col=$((j + dir_col))
-        
-                if [[ next_row -ge 0 ]] && [[ next_row -lt rows ]] && [[ next_col -ge 0 ]] && [[ next_col -lt cols ]]; then
-                    char=${grid[$next_row]:$next_col:1}
-                    if [[ "$char" == "@" ]]; then
-                        ((count++))
-                    fi
-                fi
-            done
-            
-            if [[ count -lt 4 ]]; then
-                ((accessible++))
-            fi
-        fi
-    done
+while [[ "$reset" == "true" ]]; do
+	reset="false"
+
+	for ((i=0; i<rows; i++)); do
+	    for ((j=0; j<cols; j++)); do
+	        char=${grid[$i]:$j:1}
+	        
+	        count=0
+	        if [[ "$char" == "@" ]]; then
+	            for direction in "${directions[@]}"; do
+	                read dir_row dir_col <<< "$direction"
+	                next_row=$((i + dir_row))
+	                next_col=$((j + dir_col))
+	        
+	                if [[ next_row -ge 0 ]] && [[ next_row -lt rows ]] && [[ next_col -ge 0 ]] && [[ next_col -lt cols ]]; then
+	                    char=${grid[$next_row]:$next_col:1}
+	                    if [[ "$char" == "@" ]]; then
+	                        ((count++))
+	                    fi
+	                fi
+	            done
+	            
+	            if [[ count -lt 4 ]]; then
+	                ((accessible++))
+	                grid[$i]="${grid[$i]:0:$j}.${grid[$i]:$((j+1))}"
+	                reset="true"
+	            fi
+	        fi
+	    done
+	done
 done
 
 echo "The number of accessible rolls of paper is: $accessible."
 
+# debug
+# for ((i=0;i<rows; i++)); do
+#     echo ${grid[$i]}
+# done
