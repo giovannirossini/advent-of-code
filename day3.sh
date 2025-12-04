71d9caf14e3a7238880aeea65886f66481c1fafb
Giovanni Rossini <giovannijrrossini@gmail.com>
2025-12-03 23:41:56 -0300

    day3: part two

--- day3.sh
+++ day3.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-sum=0
+sum_one=0
+sum_two=0
 
 while read bank; do
     max=0
     len=${#bank}
+    keep=12
+    result=""
+    skip=$((len-keep))
 
     echo "$((++count))/$(wc -l $1 | awk '{print $1}')"
-    for ((i=0; i<len-1; i++)); do
+    for ((i=0; i<len; i++)); do
         left=${bank:i:1}
 
         for ((j=i+1; j<len; j++)); do
@@ -18,9 +22,22 @@ while read bank; do
                 max=$num
             fi
         done
+
+        while [[ ${#result} -gt 0 ]] && [[ skip -gt 0 ]] && [[ ${result: -1} -lt left ]]; do
+            result=${result%?}
+            ((skip--))
+        done
+
+        if [[ ${#result} -lt keep ]]; then
+            result=${result}${left}
+        else
+            ((skip--))
+        fi
     done
 
-    ((sum+=max))
+    ((sum_one+=max))
+    ((sum_two+=result))
 done < $1
 
-echo "The total joltage output is: $sum."
+echo "The total joltage output for part one is: $sum_one."
+echo "The total joltage output for part two is: $sum_two."
