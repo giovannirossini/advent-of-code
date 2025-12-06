ae9a758594b07fb788c82fdebac04e561d35bec5
Giovanni Rossini <giovannijrrossini@gmail.com>
2025-12-06 19:35:12 -0300

    day6: part two

--- day6.sh
+++ day6.sh
@@ -1,18 +1,75 @@
 #!/bin/bash
 
+
 input=$1
 columns=$(head -n1 $input | wc -w)
-sum=0
 
-for ((i=1;i<=$columns; i++)); do
+function part-one() {
     set -f
-    column=($(awk -v idx="$i" '{print $idx}' "$input"))
-    last=$((${#column[@]}-1))
-    operation=${column[$last]}
-    unset column[$last]
-    math=$(printf " %s ${operation}" "${column[@]}" | awk '{$NF=""; print}')
-    sum=$((sum+math))
+    local sum
+    for ((i=1;i<=$columns; i++)); do
+        column=($(awk -v idx="$i" '{print $idx}' "$input"))
+        last=$((${#column[@]}-1))
+        operation=${column[$last]}
+        unset column[$last]
+        math=$(printf " %s ${operation}" "${column[@]}" | awk '{$NF=""; print}')
+        sum=$((sum+math))
+    done
     set +f
-done
+    echo -e "[part one] The total grand is: $sum"
+}
 
-echo -e "[part one] The total grand is: $sum"
+function part-two() {
+    set -f
+
+    lines=()
+    while IFS='' read -r line || [[ -n $line ]]; do
+        lines+=("$line")
+    done < "$input"
+
+    max_len=0
+    for line in "${lines[@]}"; do
+        (( ${#line} > max_len )) && max_len=${#line}
+    done
+
+    total=0
+    nums=()
+
+    for ((col=0; col<=max_len; col++)); do
+        vertical=""
+        col_is_empty=1
+        
+        for ((r=0; r<${#lines[@]}; r++)); do
+            char="${lines[$r]:$col:1}" 
+            
+            if [[ "$char" == "+" ]] || [[ "$char" == "*" ]]; then
+                operation="$char"
+                col_is_empty=0
+            elif [[ "$char" =~ [0-9] ]]; then
+                vertical="${vertical}${char}"
+                col_is_empty=0
+            fi
+        done
+
+        if [[ col_is_empty ]] && [[ -n "$vertical" ]]; then
+             nums+=("$vertical")
+        else
+            if [[ ${#nums[@]} -gt 0 ]]; then
+                result=${nums[0]}
+                
+                for ((i=1; i<${#nums[@]}; i++)); do
+                        math="$result $operation ${nums[$i]}"
+                        result=$((math))
+                done
+                
+                total=$((total + result))
+                nums=()
+            fi
+        fi
+    done
+    set +f
+    echo "[part two] The total grand is: $total"
+}
+
+part-one
+part-two
